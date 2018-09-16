a94becde178d1e50a7d44f233fc1788054fa198f
bishalsapkota <bishalsap2012@gmail.com>
2018-09-16 22:15:03 +0545

    Added login form

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html> 
+<html lang="en">
+   <head>
+    <meta name="description" content="PHP login system" />
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <meta name="keywords" content="Responsive Business Template" />
+    <meta name="author" content="Bishal Sapkota" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      
+      <title>PHP Login System</title>
+
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.14/css/uikit.min.css" />
+
+   </head>
+
+    <body>
+
+    	<div class="uk-section uk-container">
+    		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+    			<form class="uk-form-stacked">
+
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">Email</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+                        </div>
+                    </div>
+
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-password">Password</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input" id="form-stacked-password" type="password" required="required" placeholder="your password">
+                        </div>
+                    </div>
+
+                    <div class="uk-margin">
+                        <button class="uk-button uk-button-default" type="submit">Login</button>
+                    </div>
+
+                </form>
+    		</div>
+    	</div>
+        
+    <!-- JQuery -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- UIkit JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.14/js/uikit.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.14/js/uikit-icons.min.js"></script>
+
+    </body>
+
+</html>
